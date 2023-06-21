1ff6e94cd90f1ce9f752eb728a3aaddcb661ce46
Caoimhe.Lynch.76010228 <Caoimhe.Lynch@microfocus.com>
2023-06-21 12:08:39 +0100

    junit dependency change

--- worker-message-prioritization-target-capacity-calculators/src/test/java/com/github/workerframework/workermessageprioritization/redistribution/TunedTargetQueueLengthProviderTest.java
+++ worker-message-prioritization-target-capacity-calculators/src/test/java/com/github/workerframework/workermessageprioritization/redistribution/TunedTargetQueueLengthProviderTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2022-2023 Open Text.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.workerframework.workermessageprioritization.redistribution;
 
 import com.github.workerframework.workermessageprioritization.targetqueue.TunedTargetQueueLengthProvider;
@@ -6,11 +21,11 @@ import com.github.workerframework.workermessageprioritization.targetqueue.Target
 import com.github.workerframework.workermessageprioritization.targetqueue.RoundTargetQueueLength;
 import com.github.workerframework.workermessageprioritization.targetqueue.PerformanceMetrics;
 import org.junit.Test;
-import org.mockito.ArgumentMatchers;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import org.mockito.ArgumentMatchers;
 
 public class TunedTargetQueueLengthProviderTest {
 
