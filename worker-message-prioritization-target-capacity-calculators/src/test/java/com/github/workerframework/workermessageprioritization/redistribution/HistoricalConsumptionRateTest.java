7d6bb5fdb2384d565720b2c210b45bb06e8951f1
caoimhelynch <59800744+caoimhelynch@users.noreply.github.com>
2023-07-03 12:45:37 +0100

    Update worker-message-prioritization-target-capacity-calculators/src/test/java/com/github/workerframework/workermessageprioritization/redistribution/HistoricalConsumptionRateTest.java
    Co-authored-by: andyreidz <andrew.reid@microfocus.com>

--- worker-message-prioritization-target-capacity-calculators/src/test/java/com/github/workerframework/workermessageprioritization/redistribution/HistoricalConsumptionRateTest.java
+++ worker-message-prioritization-target-capacity-calculators/src/test/java/com/github/workerframework/workermessageprioritization/redistribution/HistoricalConsumptionRateTest.java
@@ -79,7 +79,7 @@ public class HistoricalConsumptionRateTest {
 
         final boolean isSufficientHistoryAvailableQueue1 = historicalConsumptionRate.isSufficientHistoryAvailable(targetQueue1);
 
-        final boolean theoreticalConsumptionRateHistoryQueue2 = historicalConsumptionRate.isSufficientHistoryAvailable(targetQueue2);
+        final boolean isSufficientHistoryAvailableQueue2 = historicalConsumptionRate.isSufficientHistoryAvailable(targetQueue2);
 
         assertTrue("Should return true as this is set to provide the minimum consumption rate history.",
                 theoreticalConsumptionRateHistoryQueue1);
