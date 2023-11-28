bba32304d72f7ca3686f34db504ea92408d32459
jonnymccoubrey <jonathan.mccoubrey@microfocus.com>
2023-11-28 10:12:59 +0000

    Revert "Set rabbit port to 0 by default"
    This reverts commit 9017716104545cd64aa7c96380fc7cb91ba086c5.

--- worker-message-prioritization-distribution/src/main/java/com/github/workerframework/workermessageprioritization/redistribution/DistributorModule.java
+++ worker-message-prioritization-distribution/src/main/java/com/github/workerframework/workermessageprioritization/redistribution/DistributorModule.java
@@ -157,7 +157,7 @@ public class DistributorModule extends AbstractModule {
     ConnectionFactory provideConnectionFactory(final MessageDistributorConfig messageDistributorConfig) {
         ConnectionFactory connectionFactory = new ConnectionFactory();
 
-        if (messageDistributorConfig.getRabbitMQHost() == null && messageDistributorConfig.getRabbitMQPort() == 0) {
+        if (messageDistributorConfig.getRabbitMQHost() == null && messageDistributorConfig.getRabbitMQPort() == null) {
             try {
                 connectionFactory.setUri(messageDistributorConfig.getRabbitMQUrl());
             } catch (URISyntaxException | NoSuchAlgorithmException | KeyManagementException e) {
